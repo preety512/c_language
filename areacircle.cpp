1374a23933d4978b8d3b134fd1b5877a935e0fdd
preety yadav <139059606+preety512@users.noreply.github.com>
2023-11-30 18:17:09 +0000

    Add files via upload

---
+++ areacircle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	float PI=3.14,area;
+	float r;
+	printf("enter any number to find area of circle:");
+	scanf("%f",&r);
+	area=PI*r*r;
+	printf("area of circle is :%f",area);
+	
+
+}
