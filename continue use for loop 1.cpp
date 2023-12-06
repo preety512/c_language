101f92665280e1ad57c3a951f81ee4b2757a0dbc
preety yadav <139059606+preety512@users.noreply.github.com>
2023-12-06 13:57:00 +0000

    Add files via upload

---
+++ continue use for loop 1.cpp
@@ -0,0 +1,16 @@
+//continue use of for loop
+// break use in for loop
+#include<stdio.h>
+main()
+{
+	int i;
+	for(i=1;i<=10;i++){
+		if(i==5)
+
+		{
+			continue;
+		}
+			printf(" %d ",i);
+		
+}
+}
