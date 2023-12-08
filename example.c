ea4a4362b5644e99603f517225a41f7a0e1dc0d3
preety yadav <139059606+preety512@users.noreply.github.com>
2023-12-08 16:38:29 +0000

    Add files via upload

---
+++ example.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+int a,b;
+printf("enter the two any number ");
+scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		printf("difference is%d ",a-b);
+		
+	}
+	else{
+		printf("sum is=%d",a+b);
+	}
+
+}
