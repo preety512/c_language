d94658ef98a3f6d979b70fbd73b94c55f999fd54
preety yadav <139059606+preety512@users.noreply.github.com>
2023-12-08 16:37:40 +0000

    Add files via upload

---
+++ even or odd.cpp
@@ -0,0 +1,14 @@
+//wap to check whether given number is even or odd
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	a%2==0?printf("this number is even"):printf("this number is odd");
+}
+
+
+
+
+
