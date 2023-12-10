3681aa6d47aa933a772240e2df2943c9cecc6f31
preety yadav <139059606+preety512@users.noreply.github.com>
2023-12-10 16:19:01 +0000

    Add files via upload

---
+++ for task 2.cpp
@@ -0,0 +1,10 @@
+//2,5,10,20,40,60,80,160
+#include<stdio.h>
+main()
+{
+	int i;
+	for(i=5;i<=169;i*=2)
+	{
+		printf(" %d ",i);
+	}
+}
---
+++ for task 3.cpp
@@ -0,0 +1,10 @@
+//100,80,60,40,20,0
+#include<stdio.h>		
+main()
+{
+	int i;
+	for(i=100;i>=0;i-=20)
+	{
+		printf(" %d ",i);
+	}
+}
